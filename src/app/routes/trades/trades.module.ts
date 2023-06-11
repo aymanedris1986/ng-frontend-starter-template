db85d3b7d9ab36b293093c417146ab90a5587f36
Ayman Edris <ayman.edris1986@gmail.com>
2023-06-11 09:08:25 +0300

    c

--- src/app/routes/trades/trades.module.ts
+++ src/app/routes/trades/trades.module.ts
@@ -3,6 +3,9 @@ import { SharedModule } from '@shared/shared.module';
 import { TradesRoutingModule } from './trades-routing.module';
 import { TradesNewPositionComponent } from './new-position/new-position.component';
 import { TradesTradeEditFormComponent } from './trade-edit-form/trade-edit-form.component';
+import {BadgeModule} from 'primeng/badge';
+import {ButtonModule} from 'primeng/button';
+
 
 const COMPONENTS: any[] = [TradesNewPositionComponent, TradesTradeEditFormComponent];
 const COMPONENTS_DYNAMIC: any[] = [];
@@ -10,7 +13,9 @@ const COMPONENTS_DYNAMIC: any[] = [];
 @NgModule({
   imports: [
     SharedModule,
-    TradesRoutingModule
+    TradesRoutingModule,
+    BadgeModule,
+    ButtonModule
   ],
   declarations: [
     ...COMPONENTS,
