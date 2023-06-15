fae700f66476af1db07dcd08b23633edb9792c38
Ayman Edris <ayman.edris1986@gmail.com>
2023-06-15 09:01:15 +0300

    c

--- src/app/routes/trades/trade-bulk-editor/trade-bulk-editor.component.ts
+++ src/app/routes/trades/trade-bulk-editor/trade-bulk-editor.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Trade} from '@shared/model/trade';
+import {StepperSelectionEvent} from '@angular/cdk/stepper';
+import {TradesTradeEditFormComponent} from '../trade-edit-form/trade-edit-form.component';
 
 @Component({
   selector: 'app-trades-trade-bulk-editor',
@@ -9,11 +11,29 @@ import {Trade} from '@shared/model/trade';
 export class TradesTradeBulkEditorComponent implements OnInit {
 
   @Input()bulkEditTrades:Trade[] = [];
+  currentIndex = 0;
+
+  @ViewChildren(TradesTradeEditFormComponent) form: QueryList<TradesTradeEditFormComponent>
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  selectionChange(event:StepperSelectionEvent){
+    this.currentIndex = event.selectedIndex;
+    this.form.get(event.previouslySelectedIndex)?.submit();
+  }
+
+  get currentForm():TradesTradeEditFormComponent | undefined{
+    return  ;
+  }
+
+  isHasError(index:number):boolean{
+    return !this.form?.get(index)?.fg.valid;
+  }
+
 
 
 }
