cc33653a55e82f85463df9ef3ea19a7024e37f13
Ayman Edris <ayman.edris1986@gmail.com>
2023-06-17 11:03:46 +0300

    c

--- src/app/theme/widgets/user.component.ts
+++ src/app/theme/widgets/user.component.ts
@@ -8,23 +8,23 @@ import { debounceTime, tap } from 'rxjs/operators';
   selector: 'app-user',
   template: `
     <button class="r-full" mat-button [matMenuTriggerFor]="menu">
-      <img matButtonIcon class="avatar r-full" [src]="user.avatar" width="24" alt="avatar" />
+<!--      <img matButtonIcon class="avatar r-full" [src]="user.avatar" width="24" alt="avatar" />-->
       <span class="m-x-8">{{ user.name }}</span>
     </button>
 
     <mat-menu #menu="matMenu">
-      <button routerLink="/profile/overview" mat-menu-item>
-        <mat-icon>account_circle</mat-icon>
-        <span>{{ 'profile' | translate }}</span>
-      </button>
-      <button routerLink="/profile/settings" mat-menu-item>
-        <mat-icon>edit</mat-icon>
-        <span>{{ 'edit_profile' | translate }}</span>
-      </button>
-      <button mat-menu-item (click)="restore()">
-        <mat-icon>restore</mat-icon>
-        <span>{{ 'restore_defaults' | translate }}</span>
-      </button>
+<!--      <button routerLink="/profile/overview" mat-menu-item>-->
+<!--        <mat-icon>account_circle</mat-icon>-->
+<!--        <span>{{ 'profile' | translate }}</span>-->
+<!--      </button>-->
+<!--      <button routerLink="/profile/settings" mat-menu-item>-->
+<!--        <mat-icon>edit</mat-icon>-->
+<!--        <span>{{ 'edit_profile' | translate }}</span>-->
+<!--      </button>-->
+<!--      <button mat-menu-item (click)="restore()">-->
+<!--        <mat-icon>restore</mat-icon>-->
+<!--        <span>{{ 'restore_defaults' | translate }}</span>-->
+<!--      </button>-->
       <button mat-menu-item (click)="logout()">
         <mat-icon>exit_to_app</mat-icon>
         <span>{{ 'logout' | translate }}</span>
